43a8f51f256cce92209f3f6ea8e21bf68b3a06df
matias abad <matiasabadguerra18@hotmail.com>
2022-06-15 21:12:29 -0300

    desafio 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React from 'react'
+import Navbar from './componentes/header/Navbar';
 import './App.css';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          nuevo proyecto
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+    </>
+    
+      
+
+
+    
+    
+    
+    
   );
 }
 
